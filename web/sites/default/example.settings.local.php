c0e2d8be3a2b97e04e98df08b218a6714d7070cd
Steve Zipfel <steve@studiosixthirteen.com>
2023-01-30 16:01:22 -0500

    Add nupdates, config etc.

---
+++ web/sites/default/example.settings.local.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'default',
+  'username' => 'user',
+  'password' => 'user',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
